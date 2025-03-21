1bf7b771296f6bdf45a18ab79f87dce4a0d2f406
Sanuda Senod <ypsspathiraja@std.appsc.sab.ac.lk>
2025-03-21 18:18:16 +0530

    few chnagesd added

--- src/main/java/com/example/stringmanipulationservice/StringManipulationService.java
+++ src/main/java/com/example/stringmanipulationservice/StringManipulationService.java
@@ -1,32 +1,3 @@
-// package com.example.stringmanipulationservice;
-
-// import jakarta.jws.WebMethod;
-// import jakarta.jws.WebService;
-
-// @WebService
-// public class StringManipulationService {
-//     // Reverse the input text
-//     @WebMethod
-//     public String reverseText(String text) {
-//         // StringBuilder sb = new StringBuilder(text);
-//         // return sb.reverse().toString();
-//         System.out.println("Received text: " + text); // Log the input
-//         if (text == null) {
-//             throw new IllegalArgumentException("Input text cannot be null");
-//         }
-//         StringBuilder sb = new StringBuilder(text);
-//         return sb.reverse().toString();
-//     }
-
-//     // Count characters in the input text
-//     @WebMethod
-//     public int countCharacters(String text) {
-//         return text != null ? text.length() : 0;
-//     }
-// }
-
-
-
 package com.example.stringmanipulationservice;
 
 import jakarta.jws.WebMethod;
